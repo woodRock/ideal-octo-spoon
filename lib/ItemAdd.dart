15c48ab43b052742bf68e9247a1422c141e9b034
woodRock <j.r.h.wood98@gmail.com>
2020-11-10 20:04:18 +1300

    Refactor screens to have subcomponents

--- lib/ItemAdd.dart
+++ lib/ItemAdd.dart
@@ -8,45 +8,49 @@ class ItemAdd extends StatelessWidget {
         appBar: AppBar(
             title: Text('Add')
         ),
-        body: Column(
-          children: [
-            Expanded(
-                child: Form(
-                  key: _formKey,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      TextFormField(
-                        decoration: InputDecoration(
+        body: getForm(context, _formKey)
+    );
+  }
+
+  Widget getForm(context, _formKey) {
+    return Column(
+      children: [
+        Expanded(
+            child: Form(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    TextFormField(
+                      decoration: InputDecoration(
                           border: InputBorder.none,
                           labelText: 'Item name'
-                        ),
-                        validator: (value) {
-                          return (value.isEmpty)? 'Please a name' : null;
-                        },
                       ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 16.0),
-                        child: ElevatedButton(
-                          onPressed: () {
-                            if (_formKey.currentState.validate()) {
-                              Scaffold.of(context).showSnackBar(
-                                  SnackBar(content: Text('Processing Data')));
-                            }
-                          },
-                          child: Text('Submit'),
-                        ),
-                      )
-                    ],
-                  )
-              )
-            ),
-            RaisedButton(
-                child: Text('Back'),
-                onPressed: () => Navigator.pushNamed(context, '/'),
+                      validator: (value) {
+                        return (value.isEmpty)? 'Please a name' : null;
+                      },
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 16.0),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          if (_formKey.currentState.validate()) {
+                            Scaffold.of(context).showSnackBar(
+                                SnackBar(content: Text('Processing Data')));
+                          }
+                        },
+                        child: Text('Submit'),
+                      ),
+                    )
+                  ],
+                )
             )
-          ],
+        ),
+        RaisedButton(
+          child: Text('Back'),
+          onPressed: () => Navigator.pushNamed(context, '/'),
         )
+      ],
     );
   }
 }
--- lib/ItemList.dart
+++ lib/ItemList.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class ItemList extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     // TODO Implement context to store the items
@@ -9,25 +10,29 @@ class ItemList extends StatelessWidget {
         appBar: AppBar(
           title: Text('List')
         ),
-        body: Column(
-          children: [
-            Expanded(
-                child: ListView.builder(
-                  itemCount: items.length,
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                        leading: Icon(Icons.emoji_food_beverage),
-                        title : Text('${items[index]}')
-                    );
-                  },
-                ),
-            ),
-            RaisedButton(
-              child: Text('Add'),
-              onPressed: () => Navigator.pushNamed(context, '/add'),
-            )
-          ],
+        body: getList(context, items)
+    );
+  }
+
+  Widget getList(context, items) {
+    return Column(
+      children: [
+        Expanded(
+          child: ListView.builder(
+            itemCount: items.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                  leading: Icon(Icons.emoji_food_beverage),
+                  title : Text('${items[index]}')
+              );
+            },
+          ),
+        ),
+        RaisedButton(
+          child: Text('Add'),
+          onPressed: () => Navigator.pushNamed(context, '/add'),
         )
+      ],
     );
   }
 }
