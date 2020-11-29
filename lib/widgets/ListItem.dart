44ee1ca69d587d27692f3331b9a9b4293ef2ac71
woodRock <j.r.h.wood98@gmail.com>
2020-11-29 21:17:18 +1300

    Refactor ListItem from a monolith, into subcomponents. This improves the readability of the class.

--- lib/widgets/ListItem.dart
+++ lib/widgets/ListItem.dart
@@ -17,73 +17,101 @@ class ListItem extends StatelessWidget {
   Widget build(BuildContext context) {
     Item item = Provider.of<ItemsModel>(context).get(this._index);
     return Dismissible(
-      key: Key(item.name),
-      background: Container(
-        color: Colors.green,
-        padding: EdgeInsets.symmetric(horizontal: 20),
-        alignment: AlignmentDirectional.centerStart,
-        child: Icon(
-          Icons.edit,
-          color: Colors.white,
-        ),
+        key: Key(item.name),
+        background: editSide(),
+        secondaryBackground: deleteSide(),
+        onDismissed: (direction) {
+          switch (direction) {
+            case DismissDirection.startToEnd:
+              editItem(context, item);
+              break;
+            case DismissDirection.endToStart:
+              deleteItem(context, item);
+              break;
+            default:
+              break;
+          }
+        },
+        child: ListTile(
+          leading: count(context, item),
+          title: label(context, item),
+        ));
+  }
+
+  /// Takes a user to the edit screen for an item.
+  void editItem(BuildContext context, Item item) {
+    Item ref = item;
+    Provider.of<ItemsModel>(context).delete(item);
+    Navigator.push(context, MaterialPageRoute(builder: (context) => Edit(ref)));
+  }
+
+  /// Removes an item from the stock entirely.
+  /// Briefly provide the opportunity to undo.
+  void deleteItem(BuildContext context, Item item) {
+    Provider.of<ItemsModel>(context).delete(item);
+    Scaffold.of(context).showSnackBar(SnackBar(
+      content: Text('${item.name} removed from stock'),
+      duration: Duration(seconds: 2),
+      action: SnackBarAction(
+        label: 'Undo',
+        textColor: Colors.yellow,
+        onPressed: () => Provider.of<ItemsModel>(context).add(item),
       ),
-      secondaryBackground: Container(
-        color: Colors.red,
-        padding: EdgeInsets.symmetric(horizontal: 20),
-        alignment: AlignmentDirectional.centerEnd,
-        child: Icon(
-          Icons.delete,
-          color: Colors.white,
-        ),
-      ),
-      onDismissed: (direction) {
-        switch (direction) {
-          case DismissDirection.startToEnd:
-            {
-              Item ref = item;
-              Provider.of<ItemsModel>(context).delete(item);
-              Navigator.push(
-                  context, MaterialPageRoute(builder: (context) => Edit(ref)));
-            }
-            break;
-          case DismissDirection.endToStart:
-            {
-              Provider.of<ItemsModel>(context).delete(item);
-              Scaffold.of(context).showSnackBar(SnackBar(
-                content: Text('${item.name} removed from stock'),
-                duration: Duration(seconds: 2),
-                action: SnackBarAction(
-                  label: 'Undo',
-                  textColor: Colors.yellow,
-                  onPressed: () => Provider.of<ItemsModel>(context).add(item),
-                ),
-              ));
-            }
-            break;
-          default:
-            break;
-        }
-      },
-      child: ListTile(
-        leading: CircleAvatar(
-            backgroundColor: item.essential
-                ? Provider.of<ThemeModel>(context).theme
-                : Theme.of(context).secondaryHeaderColor,
-            child: RichText(
-                text: TextSpan(
-                    style: DefaultTextStyle.of(context).style,
-                    text: '${item.count}',
-                    recognizer: LongPressGestureRecognizer()
-                      ..onLongPress =
-                          () => Provider.of<ItemsModel>(context).reset(item)))),
-        title: RichText(
-            text: TextSpan(
-          style: DefaultTextStyle.of(context).style,
-          text: '${item.name}',
-          recognizer: TapGestureRecognizer()
-            ..onTap = () => Provider.of<ItemsModel>(context).increment(item),
-        )),
+    ));
+  }
+
+  /// A pencil with a green background.
+  Widget editSide() {
+    return Container(
+      color: Colors.green,
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      alignment: AlignmentDirectional.centerStart,
+      child: Icon(
+        Icons.edit,
+        color: Colors.white,
       ),
     );
+  }
+
+  /// A trash can with a red background.
+  Widget deleteSide() {
+    return Container(
+      color: Colors.red,
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      alignment: AlignmentDirectional.centerEnd,
+      child: Icon(
+        Icons.delete,
+        color: Colors.white,
+      ),
+    );
+  }
+
+  /// Displays the count for an item.
+  /// Colors infers essential or luxury item.
+  /// Reset the count with a long press on the circle.
+  Widget count(BuildContext context, Item item) {
+    return CircleAvatar(
+        backgroundColor: item.essential
+            ? Provider.of<ThemeModel>(context).theme
+            : Theme.of(context).secondaryHeaderColor,
+        child: RichText(
+            text: TextSpan(
+                style: DefaultTextStyle.of(context).style,
+                text: '${item.count}',
+                recognizer: LongPressGestureRecognizer()
+                  ..onLongPress =
+                      () => Provider.of<ItemsModel>(context).reset(item))));
+  }
+
+  /// Displays the name of the item.
+  /// Its count can be incremented by tapping its name.
+  Widget label(BuildContext context, Item item) {
+    return RichText(
+        text: TextSpan(
+            style: DefaultTextStyle.of(context).style,
+            text: '${item.name}',
+            recognizer: TapGestureRecognizer()
+              ..onTap =
+                  () => Provider.of<ItemsModel>(context).increment(item)));
   }
 }
